b3e397e20cbd62a1ecae961397714476817018f7
wu-kan <i@wu-kan.cn>
2022-04-25 13:44:36 +0000

    fix action

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -1,15 +1,13 @@
 git clone \
     -c feature.manyFiles=true \
-    --depth=1 \
     -b releases/latest \
+    --depth=1 \
     https://github.com/spack/spack $(dirname ${SCC_SETUP_ENV})/../../../spack
-(. $(dirname ${SCC_SETUP_ENV})/../../../spack/share/spack/setup-env.sh &&
-    spack compiler add --scope=site &&
-    spack repo add --scope=site $(dirname ${SCC_SETUP_ENV})/../../../sysu-scc-spack-repo &&
-    spack env create sysu-scc $(dirname ${SCC_SETUP_ENV})/../../../sysu-scc-spack-repo/spack.yaml)
+rm -rf $(dirname ${SCC_SETUP_ENV})/../../../spack/.git
 . ${SCC_SETUP_ENV}
 spack env deactivate
-spack install ${SCC_DEFAULT_COMPILER} && spack gc -y
-spack clean -a
+spack repo add --scope=site $(dirname ${SCC_SETUP_ENV})/../../../sysu-scc-spack-repo
+spack compiler add --scope=site
+spack install -y ${SCC_DEFAULT_COMPILER} && spack gc -y && spack clean -ab
 spack compiler add --scope=site $(spack location -i ${SCC_DEFAULT_COMPILER})
 spack config --scope=site add "packages:all:compiler:[${SCC_DEFAULT_COMPILER}]"
--- share/sysu-scc-spack-repo/setup-env.sh
+++ share/sysu-scc-spack-repo/setup-env.sh
@@ -1,3 +1,3 @@
 export SCC_DEFAULT_COMPILER=gcc@7.5.0
 . $(dirname ${SCC_SETUP_ENV})/../../../spack/share/spack/setup-env.sh
-spack env activate -p sysu-scc
+spack env activate -d $(dirname ${SCC_SETUP_ENV})/../../../sysu-scc-spack-repo -p sysu-scc
