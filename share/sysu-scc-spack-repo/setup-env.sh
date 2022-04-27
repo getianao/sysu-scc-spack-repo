3b7e8d838e3f95c987e7486d1c17aee83ce31591
wu-kan <i@wu-kan.cn>
2022-04-27 07:37:03 +0000

    update env

--- share/sysu-scc-spack-repo/setup-env.sh
+++ share/sysu-scc-spack-repo/setup-env.sh
@@ -1,4 +1,3 @@
 export SCC_DEFAULT_COMPILER=gcc@7.5.0
 . $(dirname ${SCC_SETUP_ENV})/../../../spack/share/spack/setup-env.sh
-spack env create sysu-scc $(dirname ${SCC_SETUP_ENV})/../../spack.yaml
 spack env activate -p sysu-scc
