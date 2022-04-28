b6ef1cab0ae5779fd0ca1c290ac83bff0dae24ae
wu-kan <i@wu-kan.cn>
2022-04-28 15:18:29 +0000

    fix action

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -8,8 +8,6 @@ rm -rf $(dirname ${SCC_SETUP_ENV})/../../../spack/.git
 . ${SCC_SETUP_ENV}
 spack repo add --scope=site $(dirname ${SCC_SETUP_ENV})/../..
 spack compiler add --scope=site
-spack env create sysu-scc $(dirname ${SCC_SETUP_ENV})/../../spack.yaml
-. ${SCC_SETUP_ENV}
 spack install -y ${SCC_DEFAULT_COMPILER} && spack gc -y && spack clean -ab
 spack compiler add --scope=site $(spack location -i ${SCC_DEFAULT_COMPILER})
 spack config --scope=site add "packages:all:compiler:[${SCC_DEFAULT_COMPILER}]"
