2fa0bdec87846a6aaacc3a14f61810c94079c64a
wu-kan <i@wu-kan.cn>
2022-11-18 18:05:26 +0800

    update sysu-lang

--- packages/sysu-lang/package.py
+++ packages/sysu-lang/package.py
@@ -10,11 +10,14 @@ class SysuLang(CMakePackage):
     """A mini, simple and modular Compiler for SYsU (a tiny C language)."""
 
     homepage = "https://github.com/arcsysu/SYsU-lang"
+    url = "https://github.com/arcsysu/SYsU-lang/archive/v11.0.7.20221118.tar.gz"
     git = homepage+".git"
 
     maintainers = ['wu-kan']
 
-    version('main',  branch='main')
+    version('11.0.7.20221118', sha256='9c7e92056f11f451a314b2f7f4684cb446235d389ded455ce1df294330fd15fd')
+    version('latest', branch='latest')
+    version('unstable-slim', branch='unstable-slim')
 
     depends_on('flex', type='build')
     depends_on('bison', type='build')
